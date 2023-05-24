b0618f956fb53257eb011e14194a2ad64a1a3bc7
Florent MARQUES <florent.marques@etu.uca.fr>
2023-05-24 23:03:30 +0200

    👷 add .drone.yml

--- Sources/UCAverageModel/Tests/UCAverageModelTests/PlayerTests.swift
+++ Sources/UCAverageModel/Tests/UCAverageModelTests/PlayerTests.swift
@@ -6,6 +6,6 @@ final class UCAverageModelTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Player().text, "Hello, World!")
+        XCTAssertEqual(Player().text, "Florent")
     }
 }
