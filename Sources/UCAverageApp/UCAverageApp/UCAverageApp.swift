7f6ee9c2543220bb5386b7aa34ae0b308c58d2b8
Florent MARQUES <florent.marques@etu.uca.fr>
2023-05-24 22:48:46 +0200

    🎉 add ios app project

---
+++ Sources/UCAverageApp/UCAverageApp/UCAverageApp.swift
@@ -0,0 +1,17 @@
+//
+//  UCAverageAppApp.swift
+//  UCAverageApp
+//
+//  Created by etudiant on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct UCAverageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
