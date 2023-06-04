cac699903a3789b66bb7aa07ad774ea073796e89
Florent MARQUES <florent.marques@etu.uca.fr>
2023-06-04 19:26:30 +0200

    :sparkles: add slider and mark slider

---
+++ Sources/UCAverageApp/UCAverageApp/Components/SimpleSlider.swift
@@ -0,0 +1,50 @@
+//
+//  SimpleSlider.swift
+//  UCAverageApp
+//
+//  Created by etudiant on 04/06/2023.
+//
+
+import SwiftUI
+
+// slider from https://stackoverflow.com/a/58288003
+struct SimpleSlider: View {
+    @Binding var value: Float
+    @Binding var isEnabled: Bool
+    
+    var maxValue: Int = 20
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(.gray)
+                Rectangle()
+                    .foregroundColor(.accentColor)
+                    .clipShape(Capsule())
+                    .frame(width: geometry.size.width * CGFloat(self.value / Float(maxValue)))
+            }
+            .clipShape(Capsule())
+            .gesture(DragGesture(minimumDistance: 0)
+                .onChanged{ gesture in
+                    if isEnabled {
+                        updateValue(withGesture: gesture, andGeometry: geometry)
+                    }
+                })
+        }
+    }
+    
+    private func updateValue(withGesture gesture: DragGesture.Value, andGeometry geometry: GeometryProxy) {
+        let percent = Float(gesture.location.x / geometry.size.width)
+        let newValue: Float = percent * Float(maxValue)
+        self.value = min(max(0, newValue), Float(maxValue))
+    }
+}
+
+struct SimpleSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        SimpleSlider(value: .constant(18), isEnabled: .constant(true))
+            .frame(height: 40)
+            .previewLayout(.sizeThatFits)
+    }
+}
---
+++ Sources/UCAverageApp/UCAverageApp/Views/Courses/CourseMarkSlider.swift
@@ -0,0 +1,30 @@
+//
+//  MarkSlider.swift
+//  UCAverageApp
+//
+//  Created by etudiant on 04/06/2023.
+//
+
+import SwiftUI
+import UCAverageViewModel
+
+struct CourseMarkSlider: View {
+    @Binding var courseMark: Float
+    @Binding var isEditable: Bool
+    
+    var body: some View {
+        HStack {
+            SimpleSlider(value: $courseMark, isEnabled: $isEditable)
+                .frame(height: 20)
+            Spacer()
+            Text(String(format: "%.2f", courseMark))
+                .frame(width: 55)
+        }
+    }
+}
+
+struct CourseMarkSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseMarkSlider(courseMark: .constant(19), isEditable: .constant(true))
+    }
+}
